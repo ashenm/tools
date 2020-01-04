ee7e3449dfb55abe56d0fbc45324ba0a9edf12de
Ashen Gunaratne <mail@ashenm.ml>
2020-01-04 16:06:40 +0000

    Locally serve site in foreground

--- scripts/serve.py
+++ scripts/serve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Serve Site Locally
 
-from os import P_NOWAIT, spawnlp
+from os import P_WAIT, spawnlp
 
-print(spawnlp(P_NOWAIT, 'jekyll', 'jekyll', 'serve', '--watch', '--quiet', '--port', '8080', '--host', '0.0.0.0'))
+print(spawnlp(P_WAIT, 'jekyll', 'jekyll', 'serve', '--watch', '--quiet', '--port', '8080', '--host', '0.0.0.0'))
