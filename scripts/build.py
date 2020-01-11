3ecbec006c72384538c3f744a672543e9ba32c55
Ashen Gunaratne <mail@ashenm.ml>
2020-01-11 09:45:21 +0000

    Embed build metadata

--- scripts/build.py
+++ scripts/build.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 # Build Jekyll Site
 
+from re import sub
 from os import P_WAIT, spawnlp
+from subprocess import CalledProcessError, check_output
+from datetime import datetime
+
+try:
+  sha256 = check_output([ 'git', 'rev-parse', 'HEAD' ]).decode().strip()
+except CalledProcessError:
+  sha256 = ''
+
+with open('_includes/base.html', mode='r+t') as conffile:
+
+  configuration = conffile.read()
+
+  configuration = sub(r'<!-- macro/build: build-timestamp --!>', f'<meta name="build-timestamp" content="{datetime.utcnow().ctime()}" />', configuration)
+  configuration = sub(r'<!-- macro/build: build-commit --!>', f'<meta name="build-commit" content="{sha256}" />', configuration)
+
+  conffile.seek(0)
+  conffile.write(configuration)
 
 spawnlp(P_WAIT, 'jekyll', 'jekyll', 'build', '--profile')
