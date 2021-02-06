7bf5ce2d9ddb591914a68565f4308a2a6120b6dd
Ashen Gunaratne <mail@ashenm.ml>
2021-02-06 14:33:13 +0000

    [CI/CD] Invalidate Cloudflare caches by batches of <= 30 URIs

--- .travis/culminate.sh
+++ .travis/culminate.sh
@@ -3,11 +3,15 @@
 
 # purge CloudFlare caches
 # https://api.cloudflare.com/#zone-purge-files-by-url
-curl --silent \
-  --show-error \
-  --data "@.artifacts" \
-  --write-out "\n" \
-  --header "X-Auth-Email: ${CLOUDFLARE_USER}" \
-  --header "X-Auth-Key: ${CLOUDFLARE_TOKEN}" \
-  --header "Content-Type: application/json" \
-  --url "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"
+for PAYLOAD in $(cat .artifacts | jq --compact-output '.files |  _nwise(30) | { files: . }'); do
+
+  echo "$PAYLOAD" | jq '.'
+
+  curl --silent --show-error \
+    --data "$PAYLOAD" --write-out "\n" \
+    --header "X-Auth-Email: ${CLOUDFLARE_USER}" \
+    --header "X-Auth-Key: ${CLOUDFLARE_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --url "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"
+
+done
